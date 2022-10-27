59a74d830f0fbf07bd2b57c3d67512d36bb70130
sdk4590 <sdk4590@koreatech.ac.kr>
2022-10-27 18:00:38 +0900

    Feat.Home 중간저장-SDL

--- gganbu-front/src/components/common/navBar.vue
+++ gganbu-front/src/components/common/navBar.vue
@@ -33,6 +33,7 @@ export default {
   width: 100%;
   height: 60px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background-color: white;
 }
 .logo {
   float: left;
---
+++ gganbu-front/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="homeview"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+html {
+  background-size: cover;
+  background-image: url("../assets/img/hospital.png");
+}
+</style>
