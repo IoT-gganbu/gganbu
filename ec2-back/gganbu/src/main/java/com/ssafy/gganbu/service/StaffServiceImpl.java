cb311a173820ecf59fa724ff7cc0869a4e9fd2cc
dladndrbs <dladndrbs@naver.com>
2022-10-27 17:25:00 +0900

    Feat.progress api 완성-IWK

--- ec2-back/gganbu/src/main/java/com/ssafy/gganbu/service/StaffServiceImpl.java
+++ ec2-back/gganbu/src/main/java/com/ssafy/gganbu/service/StaffServiceImpl.java
@@ -1,13 +1,16 @@
 package com.ssafy.gganbu.service;
 
+import com.ssafy.gganbu.db.entity.PatientProgressHistory;
 import com.ssafy.gganbu.db.entity.Patients;
 import com.ssafy.gganbu.db.entity.Staffs;
+import com.ssafy.gganbu.db.repository.HistoryRepository;
 import com.ssafy.gganbu.db.repository.PatientReqository;
 import com.ssafy.gganbu.db.repository.StaffRepository;
 import com.ssafy.gganbu.exception.NoChangeExeption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.prefs.NodeChangeEvent;
 
@@ -19,6 +22,9 @@ public class StaffServiceImpl implements StaffService {
 
     @Autowired
     PatientReqository patientRepository;
+
+    @Autowired
+    HistoryRepository historyRepository;
     @Override
     public Staffs login(String id, String password) {
         System.out.println("StaffServiceImpl.login");
@@ -36,15 +42,34 @@ public class StaffServiceImpl implements StaffService {
             throw new NoSuchElementException("no patient found");
         }
         // 이미 isCheckup이 true라면?
-        if(patient.getIsCheckup() == 1){
+        if(patient.getIsCheckup()){
             throw new NoChangeExeption("aleady checked");
         }
         // isCheckup을 true로 변환
-        patient.setIsCheckup(1);
+        patient.setIsCheckup(true);
         patientRepository.save(patient);
         // 추 후에 QR코드 전송방식으로 변경해줘야함.
 
         return true;
+    }
+
+    @Override
+    public Object progress(Long patientId) {
+        System.out.println("StaffServiceImpl.progress");
+        // userId 기반으로 patient 찾기
+        Patients patient = patientRepository.findByPatientId(patientId);
+        if(patient == null) {
+            throw new NoSuchElementException("no patient found");
+        }
+        // patient 기반으로 progress 찾기
+        List<PatientProgressHistory> list = historyRepository.findByPatientOrderByTaskChecktitleAsc(patient);
+        // 정상출력 확인
+//        for(PatientProgressHistory history : list) {
+//            System.out.println(history.getTaskChecktitle().getTcId());
+//        }
+        // 제일 큰 값 하나만 리턴해준다?
+        Long max = list.get(list.size()-1).getTaskChecktitle().getTcId();
+        return max;
     }
 }
 
