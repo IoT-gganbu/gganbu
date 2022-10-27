deb09a936ee0c624390e1b7e1a7ffe7596d3f2fb
junghun95 <79126498+junghun95@users.noreply.github.com>
2022-10-27 16:11:04 +0900

    Feat: 검진 절차 안내 진행중-JJH

---
+++ gganbu-front/src/views/TestProcessView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <custom-title titleText="건강검진 절차 안내"></custom-title>
+    <div class="body">
+      <div class="row" v-for="(data, idx) in processes" :key="idx">
+        <custom-button class="col1" :btnText="data.item[0]" @click="showModal(idx, 0)"></custom-button>
+        <custom-button class="col2" :btnText="data.item[1]" @click="showModal(idx, 1)"></custom-button>
+      </div>
+    </div>
+    <custom-modal class="detail" id="detail" v-show="showImgModal" @close-modal="showImgModal = false" :titleText="process">
+      <content>
+        <process-detail :No="processNo"></process-detail>
+      </content>
+    </custom-modal>
+  </div>
+</template>
+
+<script>
+import customTitle from "@/components/common/customTitle.vue";
+import processDetail from "@/components/ProcessDetail.vue";
+export default {
+  components: {
+    customTitle,
+    processDetail,
+  },
+  data() {
+    return {
+      showImgModal: false,
+      process: "",
+      processNo: 0,
+      processes: [
+        {
+          item: ["1. 접수", "2. 기초검사/신체계측"],
+        },
+        {
+          item: ["3. 채혈, 소변 검사", "4. 흉부 방사선"],
+        },
+        {
+          item: ["5. 진찰 및 상담", "6. 자궁 경부암 검사"],
+        },
+        {
+          item: ["7. 유방암 검사", "8. 위 내시경 검사"],
+        },
+        {
+          item: ["9. 대장 내시경 검사", "10. 폐암 검사"],
+        },
+      ],
+      contents: [
+        {
+          text: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    showModal(idx, no) {
+      this.process = this.processes[idx].item[no];
+      console.log(this.process);
+      this.processNo = idx * 2 + no;
+      this.showImgModal = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  justify-content: center;
+  margin-top: 2%;
+}
+.col1,
+.col2 {
+  width: 30%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+</style>
