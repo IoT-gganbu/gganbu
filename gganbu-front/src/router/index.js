192bc62b8ee6758a09abf9dc887c8536580e865d
Seung Jun <jsznawa@gmail.com>
2022-11-01 17:56:56 +0900

    Feat. Pose Estimation 개발-JSJ

--- gganbu-front/src/router/index.js
+++ gganbu-front/src/router/index.js
@@ -4,10 +4,15 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
+  // {
+  //   path: "/",
+  //   name: "Home",
+  //   component: () => import("../views/HomeView.vue"),
+  // },
   {
     path: "/",
     name: "Home",
-    component: () => import("../views/HomeView.vue"),
+    component: () => import("../views/poseGame.vue"),
   },
   {
     path: "/process",
---
+++ gganbu-front/src/views/poseGame.vue
@@ -0,0 +1,13 @@
+<template>
+  <pose-estimation />
+</template>
+
+<script>
+import poseEstimation from "@/components/poseEstimation.vue";
+
+export default {
+  components: { poseEstimation },
+};
+</script>
+
+<style></style>
