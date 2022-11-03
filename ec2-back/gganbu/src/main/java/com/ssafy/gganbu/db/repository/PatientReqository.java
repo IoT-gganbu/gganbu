240e5f3d3fa57ea63d13f9c74bea832b6a128269
dladndrbs <dladndrbs@naver.com>
2022-11-04 08:58:16 +0900

    Fix.findAllByName(pageable) 추가-IWK

--- ec2-back/gganbu/src/main/java/com/ssafy/gganbu/db/repository/PatientReqository.java
+++ ec2-back/gganbu/src/main/java/com/ssafy/gganbu/db/repository/PatientReqository.java
@@ -1,6 +1,8 @@
 package com.ssafy.gganbu.db.repository;
 
 import com.ssafy.gganbu.db.entity.Patients;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,8 +13,12 @@ import java.util.Optional;
 public interface PatientReqository extends JpaRepository<Patients, Long> {
     // 나중에 Optional로 변경해야함.
     public Optional<Patients> findByResidentNo(String residentNo);
+
     public Optional<Boolean> existsByResidentNo(String residentNo);
+
     public Optional<List<Patients>> findAllByName(String name);
 
     public Optional<Patients> findByPatientId(Long patientId);
+
+    public Optional<Page<Patients>> findAllByName(String name, Pageable pageable);
 }
