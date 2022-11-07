c05a301a8f81a6927afff4d94712abbe7ddac61f
junghun95 <hun950803@naver.com>
2022-11-07 23:01:49 +0900

    Feat.code Refactoring-JJH

--- gganbu-back/trackings/views.py
+++ gganbu-back/trackings/views.py
@@ -7,25 +7,15 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Create your views here.
 @api_view(['GET'])
 def tracking(request):
-    # haar_upper_body_cascade = cv2.CascadeClassifier("data/haarcascade_upperbody.xml")
-    print(os.getcwd())
-    print(os.path.exists("data/haarcascade_upperbody.xml"))
+    
     casacade = cv2.CascadeClassifier("data/haarcascade_upperbody.xml")
-    # casacade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
-
-    # Uncomment this for real-time webcam detection
-    # If you have more than one webcam & your 1st/original webcam is occupied,
-    # you may increase the parameter to 1 or respectively to detect with other webcams, depending on which one you wanna use.
 
     video_capture = cv2.VideoCapture(0)
 
-    # For real-time sample video detection
-    # video_capture = cv2.VideoCapture("subway.mp4")
-    video_width = video_capture.get(3)
-    video_height = video_capture.get(4)
+    # video_width = video_capture.get(3)
+    # video_height = video_capture.get(4)
     img_counter = 0
     while True:
         ret, frame = video_capture.read()
@@ -36,8 +26,8 @@ def tracking(request):
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         box = casacade.detectMultiScale(
-            # gray,
-            rgb,
+            gray,
+            # rgb,
             scaleFactor = 1.1,
             minNeighbors = 3,
             minSize = (200, 200), # Min size for valid detection, changes according to video size or body size in the video.
@@ -65,39 +55,24 @@ def tracking(request):
     video_capture.release()
     cv2.destroyAllWindows()
 
-    def DeleteFiles(filePath):
-        if os.path.exists(filePath):
-            for file in os.scandir(filePath):
-                os.remove(file.path)
-            return 'remove_files'
-        else:
-            return 'Directory Not Found'
+    # 캡쳐한 사진 삭제 함수
+    # def DeleteFiles(filePath):
+    #     if os.path.exists(filePath):
+    #         for file in os.scandir(filePath):
+    #             os.remove(file.path)
+    #         return 'remove_files'
+    #     else:
+    #         return 'Directory Not Found'
 
     # DeleteFiles('frame_data')
 
     # 여기부터는 이미 캡쳐한거에서 상반신 박스그리기
-
     # Opening image
     img = cv2.imread("frame_data/opencv_frame_0.png")
     
-    # OpenCV opens images as BRG 
-    # but we want it as RGB We'll 
-    # also need a grayscale version
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
-    
-    # Use minSize because for not 
-    # bothering with extra-small 
-    # dots that would look like STOP signs
-
-    # stop_data = cv2.CascadeClassifier('stop_data.xml')
-    stop_data = cv2.CascadeClassifier("data/haarcascade_upperbody.xml")
-    # casacade = cv2.CascadeClassifier("data/haarcascade_upperbody.xml")
-    
-    # found = stop_data.detectMultiScale(img_rgb, 
-    #                                    minSize =(20, 20))
-
     found = casacade.detectMultiScale(
             # gray,
             rgb,
@@ -107,23 +82,13 @@ def tracking(request):
             flags = cv2.CASCADE_SCALE_IMAGE
         )
     
-    # Don't do anything if there's 
-    # no sign
     amount_found = len(found)
     
     if amount_found != 0:
-        
-        # There may be more than one
-        # sign in the image
         found_cnt = 0
         for (x, y, width, height) in found:
             found_cnt += 1
-            # We draw a green rectangle around
-            # every recognized sign
-            cv2.rectangle(img_rgb, (x, y), 
-                        (x + height, y + width), 
-                        (0, 255, 0), 5)
-            
+            cv2.rectangle(img_rgb, (x, y), (x + height, y + width), (0, 255, 0), 1)
             #  첫번째 박스만 가져오기
             #  이부분에서 여러 박스가 있을때 어떤걸 선택해야하는지 고민해봐야 한다.
             if found_cnt > 1:
@@ -140,28 +105,25 @@ def tracking(request):
 
     # 트랙커 객체 생성자 함수 리스트 ---①
     trackers = [
-        # cv2.legacy.TrackerBoosting_create,
-        #         cv2.legacy.TrackerMIL_create,
-                cv2.legacy.TrackerKCF_create,
+                # cv2.legacy.TrackerBoosting_create,
+                # cv2.legacy.TrackerMIL_create,
+                cv2.legacy.TrackerKCF_create, # 가장 성능이 좋다고 판단하여 이 모델만 사용
                 # cv2.legacy.TrackerTLD_create,
                 # cv2.legacy.TrackerMedianFlow_create,
-                # cv2.legacy.TrackerGOTURN_create, #버그로 오류 발생
                 # cv2.legacy.TrackerCSRT_create,
                 # cv2.legacy.TrackerMOSSE_create
-                # 
                 ]
-    trackerIdx = 0  # 트랙커 생성자 함수 선택 인덱스
+    trackerIdx = 0  # 트랙커 생성자 함수 선택 인덱스(우리한테는 필요가 없다.)
     tracker = None
     isFirst = True
 
     video_src = 0 # 비디오 파일과 카메라 선택 ---②
-    # video_src = "../img/highway.mp4"
     cap = cv2.VideoCapture(video_src)
     fps = cap.get(cv2.CAP_PROP_FPS) # 프레임 수 구하기
     delay = int(1000/fps)
     win_name = 'Tracking APIs'
-    returnState = 0
-    failCount = 0
+    returnState = 0 # 장고 리턴값을 유형별로 분기해서 나누어 주어야 한다.(while문이 끝나고 각각의 상태별로 리턴값을 보내줘야한다. ex)1-트래킹중 대상물체 인식실패가 3초 넘은 경우 )
+    failCount = 0 
     while cap.isOpened():
         ret, frame = cap.read()
         frame = imutils.resize(frame, width=1000)
@@ -170,12 +132,8 @@ def tracking(request):
             break
         img_draw = frame.copy()
         if tracker is None: # 트랙커 생성 안된 경우
-            # cv2.putText(img_draw, "Press the Space to set ROI!!", \
-            #     (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2,cv2.LINE_AA)
-            print("멸망")
             tracker = trackers[trackerIdx]()
         else:
-            
             ok, bbox = tracker.update(frame)   # 새로운 프레임에서 추적 위치 찾기 ---③
             (x,y,w,h) = bbox
             if ok: # 추적 성공
@@ -194,8 +152,8 @@ def tracking(request):
         trackerName = tracker.__class__.__name__
         cv2.putText(img_draw, str(trackerIdx) + ":"+trackerName , (100,20), \
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,255,0),2,cv2.LINE_AA)
-
         cv2.imshow(win_name, img_draw)
+        
         key = cv2.waitKey(delay) & 0xff
         if isFirst: 
             isFirst = False
