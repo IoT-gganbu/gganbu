f997f9fe65e7ffe1bb0b7c6c86e0aa21c6204e94
DESKTOP-KVCQHCD\SSAFY <chann585862@gmail.com>
2022-11-10 13:43:40 +0900

    Feat.FAST API CORS 해결-PC

--- gganbu-back/main.py
+++ gganbu-back/main.py
@@ -8,14 +8,25 @@ import time
 import smtplib
 from email.message import EmailMessage
 from pydantic import BaseModel
-
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 global isWaiting
 isWaiting = False
 
 class Item(BaseModel):
-    data: str
+    data: List[str]
 
 @app.get("/")
 async def root():
@@ -277,3 +288,36 @@ def checkGGanbu() :
     isWaiting = False
     return True
 
+@app.post("/email")
+def sendMail(item:Item) :
+    # STMP 서버의 url과 port 번호
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 465
+
+    # 1. SMTP 서버 연결
+    smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+
+    EMAIL_ADDR = 'ssabeulans@gmail.com'
+    EMAIL_PASSWORD = 'ymqqpiidgyefkfjq'
+
+    # 2. SMTP 서버에 로그인
+    smtp.login(EMAIL_ADDR, EMAIL_PASSWORD)
+
+    contents = item.data
+    print(len(contents))
+    print(contents[0]+" "+ contents[1])
+    # 3. MIME 형태의 이메일 메세지 작성
+    message = EmailMessage()
+    message.set_content('인플루엔자 예방접종을 매년 하십니까?\n'+contents[0]+"\n지금까지 평생 다섯갑 이상의 담배를 피운 적이 있습니까?\n"
+                        + contents[1]+"\n한 달에 몇번 음주를 하십니까?\n"+ contents[2]+ "\n부모, 형제, 자매 중에 당뇨 질환을 앓은 경우가 있습니까?\n"+contents[3])
+    message["Subject"] = "싸브란스 병원 문진 결과"
+    message["From"] = EMAIL_ADDR  #보내는 사람의 이메일 계정
+    message["To"] = 'chann585862@naver.com'
+
+    # 4. 서버로 메일 보내기
+    smtp.send_message(message)
+
+    # 5. 메일을 보내면 서버와의 연결 끊기
+    smtp.quit()
+    
+    return "SUCCESS"
