202e0de456d877e12832409f1c7e9b4bbef956bf
junghun95 <79126498+junghun95@users.noreply.github.com>
2022-11-11 11:51:55 +0900

    Feat. 웹소켓 합치기 작업중-JJH

--- gganbu-front/src/store/index.js
+++ gganbu-front/src/store/index.js
@@ -9,7 +9,11 @@ export const api = createApi();
 export default new Vuex.Store({
   state: {
     baseurl: "http://127.0.0.1:8000/",
+    springWebsocketUrl: "http://127.0.0.1:8080/ws/spring",
     patientId: "",
+    tracking: false,
+    voice: false,
+    turtleBotState: 0,
   },
   getters: {},
   mutations: {},
