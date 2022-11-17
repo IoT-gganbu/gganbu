4f88b5b5b1383f8ce419db3e50ecc8a6887a54ea
DESKTOP-KVCQHCD\SSAFY <chann585862@gmail.com>
2022-11-17 16:54:23 +0900

    Fix.1차 테스트 오류 수정, TTS 수정-PC

--- gganbu-front/src/views/PreCheckView.vue
+++ gganbu-front/src/views/PreCheckView.vue
@@ -38,26 +38,6 @@ export default {
   },
 
   methods: {
-    speak(text, opt_prop) {
-      if (typeof SpeechSynthesisUtterance === "undefined" || typeof window.speechSynthesis === "undefined") {
-        alert("이 브라우저는 음성 합성을 지원하지 않습니다.");
-        return;
-      }
-
-      window.speechSynthesis.cancel(); // 현재 읽고있다면 초기화
-
-      const prop = opt_prop || {};
-
-      const speechMsg = new SpeechSynthesisUtterance();
-      speechMsg.rate = prop.rate || 1; // 속도: 0.1 ~ 10
-      speechMsg.pitch = prop.pitch || 1; // 음높이: 0 ~ 2
-      speechMsg.lang = prop.lang || "ko-KR";
-      speechMsg.text = text;
-
-      // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
-      window.speechSynthesis.speak(speechMsg);
-    },
-
     async play() {
       const text = document.getElementById("text").innerHTML;
       const headers = {
@@ -74,13 +54,15 @@ export default {
 
     async replay() {
       const text = "다시 한번 말씀해주세요";
+      const headers = {
+        "Content-Type": "application/json",
+      };
 
       console.log(text);
-      await this.speak(text, {
-        rate: 1,
-        pitch: 1.2,
-        lang: "ko-KR",
+      this.$axios.post(this.$store.state.baseurl + "tts", { data: text }, { headers: headers }).then((response) => {
+        console.log(response);
       });
+      console.log(text);
     },
     async getData() {
       if (this.question.length == this.cnt) {
