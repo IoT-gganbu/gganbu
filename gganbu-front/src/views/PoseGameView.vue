8b0e5faef43befbc7a5280d010d3d321c9cc79fa
DESKTOP-KVCQHCD\SSAFY <chann585862@gmail.com>
2022-11-20 20:31:58 +0900

    Docs.README 추가-PC

--- gganbu-front/src/views/PoseGameView.vue
+++ gganbu-front/src/views/PoseGameView.vue
@@ -65,7 +65,7 @@ export default {
   },
   beforeDestroy() {
     console.log("pose game end");
-    clearInterval(this.startGame);
+    clearInterval(this.interval);
     this.webcam.stop();
   },
   methods: {
